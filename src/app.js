c80ab48d62d884732fead739062ce289a4a386e0
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-08-15 18:25:43 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+var cors = require('cors')
 const db = require("./db/connection")
 const PORT = process.env.PORT || 7000
 
@@ -8,6 +8,7 @@ const artiName =require("./routers/artiName")
 const app =express();
 
 
+app.use(cors())
 
 
 app.use(artiName);
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-// const DB ='mongodb+srv://eknathNagar:eknath@8900@cluster0.ilmdf.mongodb.net/EknathNagar?retryWrites=true&w=majority'
-const DB ="mongodb://localhost:27017/EknathNagar"
+const DB ='mongodb+srv://eknathNagar:eknath@8900@cluster0.ilmdf.mongodb.net/EknathNagar?retryWrites=true&w=majority'
+// const DB ="mongodb://localhost:27017/EknathNagar"
 mongoose.connect(DB,{
     useCreateIndex:true,
     useNewUrlParser:true,
